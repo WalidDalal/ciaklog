bac734c8b312df37991932e9f9d9850d4097c318
Walid Dalal <walid.dalal11@yahoo.it>
2026-06-15 03:43:50 +0200

    Setup iniziale progetto Spring Boot (Web, JPA, Security, MySQL, Lombok)

---
+++ src/main/java/com/project/ciaklog/CiaklogApplication.java
@@ -0,0 +1,13 @@
+package com.project.ciaklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiaklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiaklogApplication.class, args);
+	}
+
+}
