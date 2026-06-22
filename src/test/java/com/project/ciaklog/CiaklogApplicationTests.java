e845ea05535094536e5eb752dc9eb93e7a51fa70
WalidDalal <249173353+WalidDalal@users.noreply.github.com>
2026-06-22 13:22:27 +0200

    Add files via upload

---
+++ src/test/java/com/project/ciaklog/CiaklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ciaklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiaklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
